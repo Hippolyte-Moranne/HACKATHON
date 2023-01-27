dfda52034d021197c6cb5c3d84a163b6a8fd3cb8
Yooote <eliotreynal@orange.fr>
2023-01-27 11:53:14 +0100

    playboardsemi fonctional

--- playboard.h
+++ playboard.h
@@ -3,16 +3,29 @@
 #include "keyboard-event.h"
 
 // nombre de lignes et de colonne du board
-extern const int rows;
-extern const int columns;
+const int rows = 30;
+const int columns = 60;
 
-extern int MS; // milli-secondes
+//
+int MS = 600; // milli-secondes (200 pour aller plus vite)
 
 // vecteur représentant le board
-extern std::vector<char> board;
+std::vector<char> board = std::vector<char>(rows * columns, 'a');
 
 // ré-initialise le board
-void reset_board();
+void reset_board()
+{
+  for (int i = 0; i < board.size(); i++)
+    board[i] = ' ';
+}
 
 // dessine le board
-void draw_board();
+void draw_board()
+{
+  for (int r = 0; r < rows; r++)
+  {
+    for (int c = 0; c < columns; c++)
+      std::cout << board.at(r * columns + c);
+    std::cout << std::endl;
+  }
+}
