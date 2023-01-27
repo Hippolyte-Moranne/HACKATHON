3f33f3589b8b835037c5d6db1d357a5b7181f8ba
Hippolyte-Moranne <hippolytemoranne@hotmail.fr>
2023-01-27 11:15:25 +0100

    début escalier

---
+++ escalier.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+
+class Escalier{
+    std::vector<int> debut;
+    std::vector<int> fin;
+
+public:
+    Escalier(std::vector<int> debut, std::vector<int> fin) {}
+
+    void affichage(){
+        
+    }
+
+}
