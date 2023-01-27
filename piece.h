932c44bc73611b777efb94ac34e4e9a35008c79c
Quentin Huet <quentin.huet@etu.minesparis.psl.eu>
2023-01-27 14:49:09 +0100

    modifs pièce

--- piece.h
+++ piece.h
@@ -1,34 +1,36 @@
+#pragma once
 #include <iostream>
 #include "playboard.h"
 #include "objet.h"
-
 
 class Piece : public Objet
 {
     int haut;
     int larg;
+    int pos_x = Objet::getPosX();
+    int pos_y = Objet::getPosY();
 
 public:
-    Piece(int pos_x, int pos_y, int piece_height, int piece_width): Objet(pos_x, pos_y), haut(piece_height), larg(piece_width){}
+    Piece(int pos_x, int pos_y, int piece_height, int piece_width): pos_x(pos_x), pos_y(pos_y), haut(piece_height), larg(piece_width){}
 
-    void afficher(int pos_x, int pos_y, int piece_height, int piece_width)
+    void afficher()
     {
         // MURS
-        for(int i{0}; i < piece_height; i++)
+        for(int i{0}; i < haut; i++)
         {
             board.at((pos_y+i)*columns + pos_x) = '%';
-            board.at((pos_y+i)*columns + pos_x+piece_width) = '%';
+            board.at((pos_y+i)*columns + pos_x+larg) = '%';
         }
-        for(int i{1}; i < piece_width-1; i++)
+        for(int i{1}; i < larg-1; i++)
         {
             board.at((pos_y*columns + pos_x + i));
-            board.at((pos_y+piece_height)*columns + pos_x + i);
+            board.at((pos_y+haut)*columns + pos_x + i);
         }
 
         //INTERIEUR
-        for(int i{1}; i < piece_width - 1; i++)
+        for(int i{1}; i < larg - 1; i++)
         {
-            for (int j{1}; j <piece_height - 1; i++)
+            for (int j{1}; j <haut - 1; i++)
             {
                 board.at((pos_y+j)*columns + pos_x + i) = '.';
             }
