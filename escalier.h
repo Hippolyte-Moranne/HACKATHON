c19ddf1db9e344969c3c3a5a4f09ef6edf91311f
Hippolyte-Moranne <hippolytemoranne@hotmail.fr>
2023-01-27 13:47:48 +0100

    correction escalier

--- escalier.h
+++ escalier.h
@@ -4,6 +4,9 @@
 
 class Escalier : public Objet
 {
+    int x;
+    int y;
+
     std::vector<int> debut_couloir;
     std::vector<int> fin_couloir;
     std::vector<int> pos_couloir;
@@ -11,10 +14,10 @@ class Escalier : public Objet
     int longueur_couloir_y;
 
 public:
-    Escalier(std::vector<int> &debut, std::vector<int> &fin) : debut_couloir(debut), fin_couloir(fin),
-                                                               pos_couloir(debut) {}
+    Escalier(int abs, int ord, std::vector<int> debut, std::vector<int> fin) : 
+    Objet(abs,ord), debut_couloir(debut), fin_couloir(fin), pos_couloir(debut) {}
 
-    void affichage()
+    void afficher()
     {
         int pas_x{1};
         int pas_y{1};
@@ -23,17 +26,19 @@ public:
         if (longueur_couloir_y < 0)
             pas_y = -1;
 
-        while ((pos_couloir[0] != fin_couloir[0]) || (pos_couloir[1] != fin_couloir))
-        {
+        while ((pos_couloir[0] != fin_couloir[0]) || (pos_couloir[1] != fin_couloir[1]))
+        {// !! boucle infinie ? 
             while ((pos_couloir[0] != fin_couloir[0]) &&
-                   (nom_du_board[pos_couloir[0] + pas_x + columns * pos_couloir[1]] == ' '))
+                   (board[pos_couloir[0] + pas_x + columns * pos_couloir[1]] == ' '))
             {
                 pos_couloir[0] += pas_x;
+                board.at(pos_couloir[0] + columns * pos_couloir[1])='#';
             }
             while ((pos_couloir[1] != fin_couloir[1]) &&
-                   (nom_du_board[pos_couloir[0] + columns * (pos_couloir[1] + pas_y)] == ' '))
+                   (board[pos_couloir[0] + columns * (pos_couloir[1] + pas_y)] == ' '))*
             {
                 pos_couloir[1] += pas_x;
+                board.at(pos_couloir[0] + columns * pos_couloir[1])='#';
             }
         }
     }
